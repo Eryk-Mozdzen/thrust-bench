4a7a717b16858791f238b020c513eef0d170f0e7
Eryk Możdżeń <ermozdzen@gmail.com>
2026-07-17 23:09:20 +0200

    merging

--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 # dependencies = [
 #     "asyncio",
 #     "aiomqtt",
-#     "pyjson",
+#     "msgpack",
 #     "datetime",
 #     "matplotlib",
 # ]
@@ -12,32 +12,44 @@
 
 import asyncio
 import aiomqtt
-import json
+import msgpack
 import struct
 import datetime
 import matplotlib.pyplot as plt
+from collections import deque
 
 
-class Queue(asyncio.Queue):
+class Bus:
     def __init__(self):
-        super().__init__()
+        self.lock = asyncio.Lock()
+        self.values = {
+            "true_thrust": None,
+            "true_torque": None,
+            "true_velocity": None,
+            "true_temperature": None,
+            "true_voltage": None,
+            "true_current": None,
+            "focus_voltage": None,
+            "focus_velocity": None,
+        }
+        self.subscribers = []
 
-        self.values = {}
-
-        self.values["thrust"] = None
-        self.values["torque"] = None
-        self.values["velocity"] = None
-        self.values["temperature"] = None
-        self.values["voltage"] = None
-        self.values["current"] = None
+    def subscribe(self):
+        q = asyncio.Queue()
+        self.subscribers.append(q)
+        return q
 
     async def put(self, item):
-        _, timestamp, data = item
+        timestamp, data = item
 
-        for key, value in data.items():
-            self.values[key] = value
+        async with self.lock:
+            self.values.update(data)
+            snapshot = self.values.copy()
 
-        await super().put((timestamp, self.values))
+        message = (timestamp, snapshot)
+
+        for q in self.subscribers:
+            await q.put(message)
 
 
 class Focus:
@@ -45,12 +57,20 @@ class Focus:
         pass
 
     async def run(self, queue):
-        async with aiomqtt.Client("localhost") as mqtt:
-            await mqtt.subscribe("focus/state")
-            async for message in mqtt.messages:
-                state = json.loads(message.payload.decode())
+        async with aiomqtt.Client("localhost") as client:
+            await client.subscribe("focus/state")
+            async for message in client.messages:
+                state = msgpack.unpackb(message.payload)
 
-                await queue.put(("focus", datetime.datetime.now(), state))
+                await queue.put(
+                    (
+                        datetime.datetime.now(),
+                        {
+                            "focus_voltage": state["supply"],
+                            "focus_velocity": state["velocity"],
+                        },
+                    )
+                )
 
 
 class Bench:
@@ -58,94 +78,119 @@ class Bench:
         pass
 
     async def run(self, queue):
-        async with aiomqtt.Client("localhost") as mqtt:
-            await mqtt.subscribe("data")
-            async for message in mqtt.messages:
+        async with aiomqtt.Client("localhost") as client:
+            await client.subscribe("data")
+            async for message in client.messages:
                 thrust, torque, velocity, temperature, voltage, current = struct.unpack(
                     "<6f", message.payload
                 )
 
                 await queue.put(
                     (
-                        "bench",
                         datetime.datetime.now(),
                         {
-                            "thrust": thrust,
-                            "torque": torque,
-                            "velocity": velocity,
-                            "temperature": temperature,
-                            "voltage": voltage,
-                            "current": current,
+                            "true_thrust": thrust,
+                            "true_torque": torque,
+                            "true_velocity": velocity,
+                            "true_temperature": temperature,
+                            "true_voltage": voltage,
+                            "true_current": current,
                         },
                     )
                 )
 
 
 class Plotter:
-    def __init__(self, signals):
+    def __init__(self, signals, max_points=100, refresh_rate=10):
         plt.ion()
 
         self.signals = signals
-        self.values = {}
+        self.max_points = max_points
+        self.refresh_period = 1.0 / refresh_rate
 
         self.fig, self.ax = plt.subplots()
 
+        self.last_draw = 0
+        self.start = datetime.datetime.now()
+
+        self.values = {}
+
         if isinstance(signals, tuple):
-            self.values[signals[0]] = []
-            self.values[signals[1]] = []
-            self.scatter = self.ax.scatter([], [])
+            self.mode = "scatter"
+
+            self.values[signals[0]] = deque(maxlen=max_points)
+            self.values[signals[1]] = deque(maxlen=max_points)
+
+            self.artist = self.ax.scatter([], [])
+
             self.ax.set_xlabel(signals[0])
             self.ax.set_ylabel(signals[1])
+
         else:
-            self.begin = datetime.datetime.now()
+            self.mode = "line"
+
             self.lines = []
+
             for s in signals:
-                self.values[s] = []
+                self.values[s] = deque(maxlen=max_points)
+
                 (line,) = self.ax.plot([], [], label=s)
                 self.lines.append(line)
-            self.ax.set_xlabel("time [s]")
+
             self.ax.legend()
+            self.ax.set_xlabel("time [s]")
 
         self.ax.grid()
-
-        plt.show()
+        plt.show(block=False)
 
     async def run(self, queue):
         while True:
             timestamp, data = await queue.get()
 
-            if hasattr(self, "lines"):
+            if self.mode == "line":
                 for s in self.signals:
-                    self.values[s].append((timestamp, data[s]))
+                    self.values[s].append(
+                        ((timestamp - self.start).total_seconds(), data[s])
+                    )
 
-            if hasattr(self, "scatter"):
+            else:
                 self.values[self.signals[0]].append(data[self.signals[0]])
                 self.values[self.signals[1]].append(data[self.signals[1]])
 
-            for key, value in self.values.items():
-                self.values[key] = value[-200:]
+            now = asyncio.get_event_loop().time()
 
-            if hasattr(self, "lines"):
-                for line, signal in zip(self.lines, self.signals):
-                    x = [
-                        (t - self.begin).total_seconds() for t, _ in self.values[signal]
-                    ]
-                    y = [y for _, y in self.values[signal]]
+            if now - self.last_draw < self.refresh_period:
+                continue
+
+            self.last_draw = now
+            self.update()
+
+    def update(self):
+        if self.mode == "line":
+
+            for line, signal in zip(self.lines, self.signals):
+                points = self.values[signal]
+
+                if points:
+                    x, y = zip(*points)
                     line.set_data(x, y)
-
-            if hasattr(self, "scatter") and (len(self.values) > 0):
-                x = [x for x in self.values[self.signals[0]]]
-                y = [y for y in self.values[self.signals[1]]]
-                self.scatter.set_offsets(list(zip(x, y)))
 
             self.ax.relim()
             self.ax.autoscale_view()
-            self.fig.canvas.draw()
-            self.fig.canvas.flush_events()
+
+        else:
+            x = self.values[self.signals[0]]
+            y = self.values[self.signals[1]]
+
+            self.artist.set_offsets(list(zip(x, y)))
+
+        self.fig.canvas.draw_idle()
+        self.fig.canvas.flush_events()
 
 
-class Writer:
-    def __init__(self, filepath):
+class Recorder:
+    def __init__(self):
+        filepath = f"recording_{datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d_%H:%M:%S")}.csv"
         self.file = open(filepath, "w")
 
     async def run(self, queue):
@@ -170,22 +215,24 @@ class Writer:
 
 
 async def main():
-    queue = Queue()
+    bus = Bus()
 
-    bench = Bench()
     focus = Focus()
-    plotter1 = Plotter(["thrust", "torque"])
-    plotter2 = Plotter(("velocity", "thrust"))
-    writer = Writer(
-        f"recording_{datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d_%H:%M:%S")}.csv"
-    )
+    bench = Bench()
+    plotter1 = Plotter(["true_velocity", "focus_velocity"])
+    plotter2 = Plotter(["true_torque"])
+    plotter3 = Plotter(("true_velocity", "true_thrust"))
+    plotter4 = Plotter(("true_velocity", "true_torque"))
+    recorder = Recorder()
 
     await asyncio.gather(
-        bench.run(queue),
-        focus.run(queue),
-        plotter1.run(queue),
-        plotter2.run(queue),
-        writer.run(queue),
+        focus.run(bus),
+        bench.run(bus),
+        plotter1.run(bus.subscribe()),
+        plotter2.run(bus.subscribe()),
+        plotter3.run(bus.subscribe()),
+        plotter4.run(bus.subscribe()),
+        recorder.run(bus.subscribe()),
     )
 
 
