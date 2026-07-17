5d7bff27851fb8665e69005c20f0aa928757266d
Eryk Możdżeń <ermozdzen@gmail.com>
2026-07-17 20:38:55 +0200

    CSV writer

--- test.py
+++ test.py
@@ -5,6 +5,7 @@
 #     "asyncio",
 #     "aiomqtt",
 #     "pyjson",
+#     "datetime",
 #     "matplotlib",
 # ]
 # ///
@@ -13,8 +14,30 @@ import asyncio
 import aiomqtt
 import json
 import struct
-import time
+import datetime
 import matplotlib.pyplot as plt
+
+
+class Queue(asyncio.Queue):
+    def __init__(self):
+        super().__init__()
+
+        self.values = {}
+
+        self.values["thrust"] = None
+        self.values["torque"] = None
+        self.values["velocity"] = None
+        self.values["temperature"] = None
+        self.values["voltage"] = None
+        self.values["current"] = None
+
+    async def put(self, item):
+        _, timestamp, data = item
+
+        for key, value in data.items():
+            self.values[key] = value
+
+        await super().put((timestamp, self.values))
 
 
 class Focus:
@@ -27,7 +50,7 @@ class Focus:
             async for message in mqtt.messages:
                 state = json.loads(message.payload.decode())
 
-                await queue.put(("focus", time.time(), state))
+                await queue.put(("focus", datetime.datetime.now(), state))
 
 
 class Bench:
@@ -45,7 +68,7 @@ class Bench:
                 await queue.put(
                     (
                         "bench",
-                        time.time(),
+                        datetime.datetime.now(),
                         {
                             "thrust": thrust,
                             "torque": torque,
@@ -74,65 +97,95 @@ class Plotter:
             self.ax.set_xlabel(signals[0])
             self.ax.set_ylabel(signals[1])
         else:
+            self.begin = datetime.datetime.now()
             self.lines = []
             for s in signals:
                 self.values[s] = []
                 (line,) = self.ax.plot([], [], label=s)
                 self.lines.append(line)
             self.ax.set_xlabel("time [s]")
+            self.ax.legend()
 
-        self.ax.legend()
         self.ax.grid()
 
         plt.show()
 
     async def run(self, queue):
         while True:
-            source, timestamp, data = await queue.get()
+            timestamp, data = await queue.get()
 
-            if source == "bench":
-                if hasattr(self, "lines"):
-                    for s in self.signals:
-                        self.values[s].append((timestamp, data[s]))
+            if hasattr(self, "lines"):
+                for s in self.signals:
+                    self.values[s].append((timestamp, data[s]))
 
-                if hasattr(self, "scatter"):
-                    self.values[self.signals[0]].append(data[self.signals[0]])
-                    self.values[self.signals[1]].append(data[self.signals[1]])
+            if hasattr(self, "scatter"):
+                self.values[self.signals[0]].append(data[self.signals[0]])
+                self.values[self.signals[1]].append(data[self.signals[1]])
 
-                for key, value in self.values.items():
-                    self.values[key] = value[-200:]
+            for key, value in self.values.items():
+                self.values[key] = value[-200:]
 
-                if hasattr(self, "lines"):
-                    for line, signal in zip(self.lines, self.signals):
-                        t0 = self.values[signal][0][0]
-                        x = [t - t0 for t, _ in self.values[signal]]
-                        y = [y for _, y in self.values[signal]]
-                        line.set_data(x, y)
+            if hasattr(self, "lines"):
+                for line, signal in zip(self.lines, self.signals):
+                    x = [
+                        (t - self.begin).total_seconds() for t, _ in self.values[signal]
+                    ]
+                    y = [y for _, y in self.values[signal]]
+                    line.set_data(x, y)
 
-                if hasattr(self, "scatter") and (len(self.values) > 0):
-                    x = [x for x in self.values[self.signals[0]]]
-                    y = [y for y in self.values[self.signals[1]]]
-                    self.scatter.set_offsets(list(zip(x, y)))
+            if hasattr(self, "scatter") and (len(self.values) > 0):
+                x = [x for x in self.values[self.signals[0]]]
+                y = [y for y in self.values[self.signals[1]]]
+                self.scatter.set_offsets(list(zip(x, y)))
 
-                self.ax.relim()
-                self.ax.autoscale_view()
-                self.fig.canvas.draw()
-                self.fig.canvas.flush_events()
+            self.ax.relim()
+            self.ax.autoscale_view()
+            self.fig.canvas.draw()
+            self.fig.canvas.flush_events()
+
+
+class Writer:
+    def __init__(self, filepath):
+        self.file = open(filepath, "w")
+
+    async def run(self, queue):
+        header = True
+
+        while True:
+            timestamp, data = await queue.get()
+
+            if header:
+                header = False
+                self.file.write("timestamp,")
+                for key in data.keys():
+                    self.file.write(f"{key},")
+                self.file.write("\n")
+
+            self.file.write(f"{timestamp},")
+            for _, value in data.items():
+                self.file.write(f"{value},")
+            self.file.write("\n")
+
+            self.file.flush()
 
 
 async def main():
-    queue = asyncio.Queue()
+    queue = Queue()
 
     bench = Bench()
     focus = Focus()
     plotter1 = Plotter(["thrust", "torque"])
     plotter2 = Plotter(("velocity", "thrust"))
+    writer = Writer(
+        f"recording_{datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d_%H:%M:%S")}.csv"
+    )
 
     await asyncio.gather(
         bench.run(queue),
         focus.run(queue),
         plotter1.run(queue),
         plotter2.run(queue),
+        writer.run(queue),
     )
 
 
